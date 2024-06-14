182d9c28d63f60fd418078056d95fc81f62524e0
Aqoom01 <rlagusgh789@daum.net>
2024-06-14 20:01:03 +0900

    Send login message & Singup UI

--- Background/Signup.cs
+++ Background/Signup.cs
@@ -12,9 +12,35 @@ namespace Background
 {
     public partial class Signup : Form
     {
-        public Signup()
+        private Form1 parent;
+        Thread m_SendThread;
+        Thread m_ReceiveThread;
+
+        public Signup(Form1 form)
         {
+            parent = form;
             InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text.Length == 0 || txtName.Text.Length == 0 || txtPwd.Text.Length == 0) return;
+
+            if(parent.m_bConnect)
+            {
+                m_SendThread = new Thread(new ThreadStart(Send));
+            }
+        }
+
+        public void Send()
+        {
+            string msg;
+        }
+
+
+        public void Receive()
+        {
+            
         }
     }
 }
