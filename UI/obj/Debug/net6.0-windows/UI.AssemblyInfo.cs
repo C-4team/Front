d1dbcddd0d74188a475bbab24f5f2f5265262bfd
seoyeon0103 <angel6462@naver.com>
2024-06-18 05:10:19 +0900

    pull reqqqest

--- UI/obj/Debug/net6.0-windows/UI.AssemblyInfo.cs
+++ UI/obj/Debug/net6.0-windows/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e10ae148b67bcfda0e7fb055105926d6cc7d5e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
