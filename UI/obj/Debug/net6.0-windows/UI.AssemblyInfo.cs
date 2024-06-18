c5a7a034574e2677ae1129881ab4e8f8a564eaa3
seoyeon0103 <angel6462@naver.com>
2024-06-18 14:27:48 +0900

    merge

--- UI/obj/Debug/net6.0-windows/UI.AssemblyInfo.cs
+++ UI/obj/Debug/net6.0-windows/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab7357bfe289816c3bd078038d119da0b04f6d3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c2654b26f1eedc15d3447bd9cc334220c769691")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
